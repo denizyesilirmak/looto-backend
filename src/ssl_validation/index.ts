bf26263109466f45254a36cc73efe4a2718759a0
denizyesilirmak <denizyesilirmak@gmail.com>
2023-07-06 16:45:13 +0300

    Add validation key

---
+++ src/ssl_validation/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const validationApp = express();
+
+validationApp.get(
+  '/.well-known/pki-validation/984FB94C7591B0795604EBB3C3AFAEE7.txt',
+  (req, res) => {
+    res.send(`
+    1A74C8929991EE39730FA7900D52AF6DF636DE552C67CC5BBF5DF869EB3A8BD9
+    comodoca.com
+    939ae1622992520
+    `);
+  }
+);
+
+export default validationApp;
