c4e9f244f5d3003926c77249d390cdd6ccbef974
denizyesilirmak <denizyesilirmak@gmail.com>
2023-07-07 11:07:09 +0300

    Update: ssl validation endpoint format

--- src/ssl_validation/index.ts
+++ src/ssl_validation/index.ts
@@ -5,11 +5,13 @@ const validationApp = express();
 validationApp.get(
   '/.well-known/pki-validation/984FB94C7591B0795604EBB3C3AFAEE7.txt',
   (req, res) => {
-    res.send(`
-    1A74C8929991EE39730FA7900D52AF6DF636DE552C67CC5BBF5DF869EB3A8BD9
-    comodoca.com
-    939ae1622992520
-    `);
+    res.send(
+      `
+1A74C8929991EE39730FA7900D52AF6DF636DE552C67CC5BBF5DF869EB3A8BD9
+comodoca.com
+939ae1622992520
+`
+    );
   }
 );
 
