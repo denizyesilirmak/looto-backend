ed54703b9fa77bf0d25290f6d5f6ec28cefce94b
yesilirmak <deniz.yesilirmak@sisal.com>
2023-07-11 17:09:01 +0300

    Add log env

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,6 +4,7 @@ import { config } from "dotenv";
 config({ path: "./test.env" });
 
 const connectDB = async () => {
+  console.log('MONGO_URI', process.env.MONGO_URI)
   mongoose.connect(process.env.MONGO_URI!).then(() => {
     console.log("✅ Database connection established.");
   });
